af82bdefd3e364b766d1e07e51450efdc73055ab
Saurav <neechanzy@gmail.com>
2024-06-10 19:36:35 +0545

    things

--- work-stuffs/src/Card.jsx
+++ work-stuffs/src/Card.jsx
@@ -2,12 +2,23 @@ import React from "react";
 
 export default function Card(props) {
     return (
-        <div className="card">
-            <img src={`${props.img}`} alt={props.title} className="card-img" />
-            <h3 className="card-title">{props.title}</h3>
-            <p className="card-location">{props.location}</p>
-            <p className="card-date">{props.startDate}</p>
-            <p className="card-description">{props.description}</p>
+        <div className="card-container">
+            <div className="main--container">
+
+            <img src={`${props.img}`} alt={props.title} className="img" />
+            </div>
+            <div className="info">
+                <div className="location">
+
+            <h6 className="location">{props.location}</h6>
+                </div>
+            
+            <h2 className="title">{props.title}</h2>
+            <h6 className="date">{props.startDate}</h6>
+            <p className="description">{props.description}</p>
+            </div>
+            
         </div>
+        
     );
 }
